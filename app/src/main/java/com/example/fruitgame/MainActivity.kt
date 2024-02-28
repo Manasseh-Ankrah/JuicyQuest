3ce3562aac426e0d04481c9514a6c9770f631cb4
Manasseh Ankrah <solomonankrah24@gmail.com>
2024-02-28 13:50:38 +0000

    Implemented View Binding

--- app/src/main/java/com/example/fruitgame/MainActivity.kt
+++ app/src/main/java/com/example/fruitgame/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.fruitgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fruitgame.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //Initializing bindings
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //Inflate the activity binding
+        binding = ActivityMainBinding.inflate(layoutInflater )
+        val view = binding.root
+        setContentView(view)
     }
 }
